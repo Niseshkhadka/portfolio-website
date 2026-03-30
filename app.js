750333162dab42575a5b6806a2445cb8b6aa5b6a
Niseshkhadka <niseshkhadka34@gmail.com>
2026-03-30 23:09:06 +0545

    created a portfolio website

---
+++ app.js
@@ -0,0 +1,10 @@
+const sendEmail = () =>{
+  window.open("mailto: niseshkhadka34@gmail.com","_blank");
+}
+
+const gitHub = () =>{
+  window.open('https://github.com/Niseshkhadka','_blank');
+}
+const linkedin=()=>{
+  window.open("https://www.linkedin.com/in/nisesh-khadka-9a533b326",'_blank');
+}
